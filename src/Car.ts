aa9292a9aa1c8c464360899985318ccbb639e624
Kolaflash <kolaflash@pop-os.localdomain>
2023-10-26 23:32:08 +0200

    WIP

--- src/Car.ts
+++ src/Car.ts
@@ -14,6 +14,7 @@ export default class Car {
   friction: number;
   angle: Angle;
   polygon: Polygon;
+  damaged: boolean;
 
   constructor(positinon: Vector2, width: number, height: number) {
     this.position = positinon;
@@ -25,13 +26,47 @@ export default class Car {
     this.acceleration = 0.2;
     this.friction = 0.1;
     this.angle = new Angle(0);
-
-    const rad = Math.hypot(height, width) / 2;
     this.polygon = new Polygon(positinon, []);
+    this.damaged = false;
   }
 
   update() {
     this.move();
+    this.updatePolygon();
+  }
+
+  updatePolygon() {
+    this.polygon.vertices = [];
+    const rad = Math.hypot(this.width, this.height) / 2;
+    const alpha = Math.atan2(this.width, this.height);
+
+    this.polygon.vertices.push(
+      new Vector2(
+        this.position.x - Math.sin(this.angle.degrees - alpha) * rad,
+        this.position.y - Math.cos(this.angle.degrees - alpha) * rad,
+      ),
+    );
+
+    this.polygon.vertices.push(
+      new Vector2(
+        this.position.x - Math.sin(this.angle.degrees + alpha) * rad,
+        this.position.y - Math.cos(this.angle.degrees + alpha) * rad,
+      ),
+    );
+
+    this.polygon.vertices.push(
+      new Vector2(
+        this.position.x - Math.sin(Math.PI + this.angle.degrees - alpha) * rad,
+        this.position.y - Math.cos(Math.PI + this.angle.degrees - alpha) * rad,
+      ),
+    );
+
+    this.polygon.vertices.push(
+      new Vector2(
+        this.position.x - Math.sin(Math.PI + this.angle.degrees + alpha) * rad,
+        this.position.y - Math.cos(Math.PI + this.angle.degrees + alpha) * rad,
+      ),
+    );
   }
 
   move() {
@@ -74,10 +109,18 @@ export default class Car {
   }
 
   draw(ctx: CanvasRenderingContext2D) {
-    ctx.save();
-    ctx.translate(this.position.x, this.position.y);
-    ctx.rotate(-this.angle.degrees);
-    ctx.fillRect(-this.width / 2, -this.height / 2, this.width, this.height);
-    ctx.restore();
+    ctx.beginPath();
+    ctx.moveTo(
+      this.polygon.vertices[0].x,
+      this.polygon.vertices[0].y,
+    );
+    for (let i = 1; i < this.polygon.vertices.length; i++) {
+      ctx.lineTo(
+        this.polygon.vertices[i].x,
+        this.polygon.vertices[i].y,
+      );
+    }
+    ctx.closePath();
+    ctx.fill();
   }
 }
