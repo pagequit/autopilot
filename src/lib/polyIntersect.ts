3c5d819145a46933cb61cbbdcb51c4d2168dc48b
Kolaflash <kolaflash@pop-os.localdomain>
2023-10-29 23:49:25 +0100

    WIP

---
+++ src/lib/polyIntersect.ts
@@ -0,0 +1,21 @@
+import Polygon from "./Polygon";
+import getIntersection from "./getIntersection";
+
+export default function polyIntersect(poly1: Polygon, poly2: Polygon) {
+  for (let i = 0; i < poly1.vertices.length; i++) {
+    for (let j = 0; j < poly2.vertices.length; j++) {
+      const touch = getIntersection(
+        poly1.vertices[i],
+        poly1.vertices[(i + 1) % poly1.vertices.length],
+        poly2.vertices[j],
+        poly2.vertices[(j + 1) % poly2.vertices.length],
+      );
+
+      if (touch.offset > 0) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
